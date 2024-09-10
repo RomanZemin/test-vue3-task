709f5e5b4f11a9bf70c1591a899e220b98d15cd5
RomanZemin <RomanZemin12@yandex.ru>
2024-09-10 17:54:23 +0700

    https://github.com/RomanZemin/test-vue3-task.git

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/canban',
+  base: process.env.NODE_ENV === 'production' ? '/test-vue3-task/' : '/',
   plugins: [vue()],
 });
